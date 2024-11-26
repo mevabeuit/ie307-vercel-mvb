00291a4f10154c5db48aa9967660b17f032bf5d7
kuku12189 <kuku12189@gmail.com>
2024-11-26 22:22:33 +0700

    add seedProducts

--- api/seedProducts.js
+++ api/seedProducts.js
@@ -1,6 +1,6 @@
-import { neon } from '@neondatabase/serverless';
-import BeansData from '../data/BeansData';
-import CoffeeData from '../data/CoffeeData';
+import {neon} from '@neondatabase/serverless';
+import path from 'path';
+import fs from 'fs';
 
 const sql = neon(process.env.DATABASE_URL);
 
@@ -14,10 +14,17 @@ export default async function handler(req, res) {
     });
   }
 
-  // Hợp nhất BeansData và CoffeeData
-  const allProducts = [...BeansData, ...CoffeeData];
-
   try {
+    // Đọc dữ liệu từ các file BeansData và CoffeeData
+    const beansDataPath = path.join(process.cwd(), 'data', 'Bean.json');
+    const coffeeDataPath = path.join(process.cwd(), 'data', 'Coffee.json');
+
+    // Đọc dữ liệu từ các file JSON
+    const beansData = JSON.parse(fs.readFileSync(beansDataPath, 'utf8'));
+    const coffeeData = JSON.parse(fs.readFileSync(coffeeDataPath, 'utf8'));
+
+    // Hợp nhất BeansData và CoffeeData
+    const allProducts = [...beansData, ...coffeeData];
     // Lặp qua từng sản phẩm trong allProducts (bao gồm cả BeansData và CoffeeData)
     for (const product of allProducts) {
       // Chèn dữ liệu vào bảng products
